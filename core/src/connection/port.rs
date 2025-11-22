a1ebc40daa329a711a060e323006ade882cfc921
Roger Ortiz <roger@r0rt1z2.com>
2025-11-22 13:06:48 +0100

    core: Add more device VID/PID combinations (#24)
    Some MTK devices use vendor-specific USB IDs instead of the standard
    MediaTek ones. Add support for Sony, OPPO, and LG devices plus a few
    more MediaTek PIDs that show up in the wild.
    
    VID/PID list taken from MTKClient.
    
    Change-Id: I133a50691b47a34a5dfd62d37e22c9914f9c7236

--- core/src/connection/port.rs
+++ core/src/connection/port.rs
@@ -10,10 +10,21 @@ use crate::error::Result;
 
 /// List of all ports available for connecting and what mode they refer to.
 /// Add more entries here for vendor specific ports
+#[rustfmt::skip]
 pub const KNOWN_PORTS: &[(u16, u16, ConnectionType)] = &[
-    (0x0E8D, 0x0003, ConnectionType::Brom), // Mediatek USB Port (BROM)
+    (0x0E8D, 0x0003, ConnectionType::Brom),      // Mediatek USB Port (BROM)
+    (0x0E8D, 0x6000, ConnectionType::Preloader), // Mediatek USB Port (Preloader)
     (0x0E8D, 0x2000, ConnectionType::Preloader), // Mediatek USB Port (Preloader)
-    (0x0E8D, 0x2001, ConnectionType::Da),   // Mediatek USB Port (DA)
+    (0x0E8D, 0x2001, ConnectionType::Da),        // Mediatek USB Port (DA)
+    (0x0E8D, 0x20FF, ConnectionType::Preloader), // Mediatek USB Port (Preloader)
+    (0x0E8D, 0x3000, ConnectionType::Preloader), // Mediatek USB Port (Preloader)
+    (0x1004, 0x6000, ConnectionType::Preloader), // LG USB Port (Preloader)
+    (0x22D9, 0x0006, ConnectionType::Preloader), // OPPO USB Port (Preloader)
+    (0x0FCE, 0xF200, ConnectionType::Brom),      // Sony USB Port (BROM)
+    (0x0FCE, 0xD1E9, ConnectionType::Brom),      // Sony USB Port (BROM XA1)
+    (0x0FCE, 0xD1E2, ConnectionType::Brom),      // Sony USB Port (BROM)
+    (0x0FCE, 0xD1EC, ConnectionType::Brom),      // Sony USB Port (BROM L1)
+    (0x0FCE, 0xD1DD, ConnectionType::Brom),      // Sony USB Port (BROM F3111)
 ];
 
 #[derive(Debug, PartialEq, Copy, Clone)]
